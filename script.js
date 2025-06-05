b659b68d8688450cce4439e96b41363091455a72
Joaquim Alexander Inácio Costa <RES0072862@aluno.firjansenaisesi.com.br>
2025-06-04 21:35:44 -0300

    tentando fazer scroll animation

--- script.js
+++ script.js
@@ -6,3 +6,17 @@ const body = document.body;
         });
 });
 
+// Animação on-scroll
+document.addEventListener('DOMContentLoaded', () => {
+    const animated = document.querySelectorAll('.fade-scroll');
+    const observer = new IntersectionObserver((entradas) => {
+      entradas.forEach(entrada => {
+        if (entrada.isIntersecting) {
+          entrada.target.classList.add('animate-fadeInUp');
+          observer.unobserve(entrada.target);
+        }
+      });
+    }, { threshold: 0.15 });
+  
+    animated.forEach(el => observer.observe(el));
+  });
